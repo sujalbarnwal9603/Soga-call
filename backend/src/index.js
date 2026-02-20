0512b0303eb803a069ce9a8db7a80c40a5f23ee2
Sujal Barnwal <sujalbarnwal9603@gmail.com>
2026-02-20 19:51:03 +0530

    socket io

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import {createServer} from "http";
+import {Server} from "socket.io";
+import {User} from "./models/user.model.js";
 
 dotenv.config({
     path: './.env'
@@ -8,7 +11,48 @@ dotenv.config({
 
 const PORT=process.env.PORT || 8000;
 
+const httpServer= createServer(app);
 
+const io=new Server(httpServer,{
+    cors:{
+        origin:process.env.CORS_ORIGIN,
+        credentials:true,
+    },
+});
+
+// SOCKET>IO connection handler
+io.on("connection", (socket)=>{
+    console.log("New client connected:", socket.id);
+
+    socket.on("user-online", async (userId)=>{
+        console.log(`User ${userId} is online`);
+        await User.findByIdAndUpdate(userId, {status:"online"});
+        io.emit("update-status", {userId, status:"online"});
+    });
+
+    //User goes offline manually or disconnects
+    socket.on("disconnect", async()=>{
+        console.log("Client disconnected:", socket.id);
+        // Optional: find user by socket and set offline if you track them
+    })
+
+    
+    //Handle manual logout (frontend can emit this)
+    socket.on("user-offline", async(userId)=>{
+        console.log(`User ${userId} went offline`);
+        await User.findByIdAndUpdate(userId,{status:"offline", lastSeen: Date.now()});
+        io.emit("update-status", {userId, status:"offline"});
+        
+    });
+
+    //Handle manual logout
+
+
+})
+
+
+
+// Start the server after connecting to the database
 connectDB()
     .then(()=>{
         app.listen(PORT, () =>{
